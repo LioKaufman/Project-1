e60f7a3a16e6a658518ac88a101de98ed5bc9d2d
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-03-14 13:41:55 +0100

    game logic, cycling through questions

--- js/game.js
+++ js/game.js
@@ -11,7 +11,10 @@ var countries = [
     {question: "What is the Capital of Croatia?", answer:  "Zagreb"}
 ];
 
-function Question(country) {
+var modalContent = $("<div class='modal-content'>");
+
+
+function Question(country, number, count) {
     this.country = country;
 
     this.mainElement = $("<div class='row'>");
@@ -19,18 +22,30 @@ function Question(country) {
     this.mainElement.append(this.contentElement);
 
     this.heading = $("<h3>").text("Quiz").appendTo(this.contentElement);
+    this.counter = $("<p>").text("Question "+number+" of "+count).appendTo(this.contentElement);
     
     this.inputForm = $("<form>").appendTo(this.contentElement);
     this.formGroup = $("<div class='form-group'>");
     this.question = $("<label for='game--answer'>").text(country.question)
         .appendTo(this.formGroup);
     this.inputField = $("<input type='text' class='form-control' id='game--answer'>").appendTo(this.formGroup);
-    this.submitButton = $("<button type='submit' class='btn btn-default'>").text("Submit").appendTo(this.formGroup);
+    this.submitButton = $("<button type='button' class='btn btn-default'>").text("Submit").appendTo(this.formGroup);
     this.formGroup.appendTo(this.inputForm);
+
+    this.setCallBack = function (callback) {
+        this.submitButton.click(callback);
+    };
+
+    this.show = function () {
+        modalContent.html(this.mainElement);
+    };
 
 
 
     this.checkAnswer = function (answer) {
+        if (typeof answer === "undefined") {
+            answer = this.inputField.val();
+        }
         if (answer.toUpperCase() == this.country.answer.toUpperCase()) {
             return 1;
         } else {
@@ -40,17 +55,71 @@ function Question(country) {
 }
 
 
+function pickRandomQuestions(questionCount) {
+    var questions = [];
+    while (questions.length < questionCount) {
+        var questionIndex = Math.floor(Math.random() * countries.length);
+        nextQuestion = countries[questionIndex];
+        var unique = true;
+        questions.map(function (element) {
+            if (element.question == nextQuestion.question) {
+                unique = false;
+            }
+        });
+        if (unique) {
+            questions.push(nextQuestion);
+        }
+    }
+    return questions;
+}
+
+function startGame() {
+    var questions = pickRandomQuestions(5);
+    var questionForms = questions.map(function (question, index) {
+        return new Question(question, index + 1, questions.length);
+
+    });
+    var score = 0;
+    var firstForm = questionForms[0];
+    //console.log(firstForm);
+    for (var questionIndex = 0 ; questionIndex < questionForms.length-1 ; questionIndex++) {
+        const currentForm = questionForms[questionIndex];
+        const nextForm = questionForms[questionIndex+1];
+        console.log(questionIndex);
+        currentForm.submitButton.click(function () {
+            console.log('click!');
+            score += currentForm.checkAnswer();
+            nextForm.show();
+        })
+    }
+    firstForm.show();
+
+}
+
+
+
+/*
 function gameLoop(questionCount) {
     var points = 0;
-    
+
+    var questions = pickRandomQuestions(questionCount);
+    var firstQuestion = new Question(questions[0], 1, questions.length);
+    firstQuestion.setCallBack(function () {
+
+    });
+    firstQuestion.show();
+
+    })
 
 
 }
-var modalContent = $("<div class='modal-content'>");
+*/
 
 var startGameButton = $("<button type='button' class='btn btn-primary' data-toggle='modal' data-target='.game--modal'>").text(" Discount Game! ");
 startGameButton.click(function () {
-    modalContent.html(new Question(countries[0]).mainElement);
+    //testForm = new Question(countries[0], 1, 5);
+    //testForm.show();
+    startGame();
 });
 
 var gameModal = $("<div class='modal fade game--modal' tabindex='-1' role='dialog' aria-labelledby='myLargeModalLabel'>").append($("<div class='modal-dialog modal-lg' role='document'>").append(modalContent));

093bb559c5ff7a1d29087303761ccbd4a7976aa3
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-03-15 10:20:28 +0100

    add total discount calculation

--- js/booking.js
+++ js/booking.js
@@ -1,3 +1,29 @@
 /**
  * Created by niki on 15.03.17.
  */
+
+/**
+ *  If customer's first name starts with C, L, H, I, G  they should immediately get
+ *  discount of 20% and they can combine this discount with game discount.
+ * @param firstName
+ * @param discountCode
+ */
+function calculateDiscount(firstName, discountCode) {
+    var discount = 0;
+
+    var firstNameBegin = firstName.charAt(0).toUpperCase();
+    switch (firstNameBegin) {
+        case "C":
+        case "L":
+        case "H":
+        case "I":
+        case "G":
+            discount += 0.2;
+            break;
+        default:
+            break;
+    }
+    discount +=  getDiscount(discountCode);
+
+    return discount;
+}

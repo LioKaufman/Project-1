75163fa6ba98fe788e2db946260efca56ed93abc
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-03-16 09:45:14 +0100

    offers testimonials carousel

--- js/carousel.js
+++ js/carousel.js
@@ -98,9 +98,87 @@ destinations.map(function (destination) {
 });
 **/
 
+
+var mainCarouselDestinations = new Carousel("mainCarouselDestinations");
+mainCarouselDestinations.addControl('prev');
+mainCarouselDestinations.addControl('next');
+destinations.map(function (destination) {
+    mainCarouselDestinations.addItem(destinationPane(destination, 'pages/booking.html', true));
+});
+
+/*
+ var offers = [
+ {Title: "Thailand", MinNights: 7, Discount: 0.22, Picture: "../thailand.jpg"},
+ {Title: "Egypt", MinNights: 4, Discount: 0.2, Picture: "../egypt.jpg"},
+ {Title: "United States", MinNights: 10, Discount: 0.1, Picture: "../usa.jpg"},
+ {Title: "Brazil", MinNights: 10, Discount: 0.15, Picture: "../brazil.jpg"},
+ {Title: "South Korea", MinNights: 6, Discount: 0.18, Picture: "../korea.jpg"}
+ ];
+*/
+function offerPane(offer, linkURL, removeDotsFromImageURL) {
+
+    var imageURL = offer.Picture;
+    console.log(offer.Picture);
+    var imageAlt = "picture of "+offer.Title;
+    var title = offer.Title;
+    var paragraph = (offer.Discount*100).toFixed()+"% off for "+offer.MinNights+"+ Nights.";
+    if ( removeDotsFromImageURL === true ) {
+        imageURL = imageURL.substring(3);
+    }
+    var element = $('<div class="item">');
+    var image = $('<img>').attr('src', imageURL).attr('alt', imageAlt);
+    var caption = $('<div class="carousel-caption">');
+    var link = $('<a>').attr('href', linkURL);
+    $('<h1>').text(title).appendTo(link);
+    $('<p>').text(paragraph).appendTo(link);
+    caption.append(link);
+    element.append(image).append(caption);
+    return element;
+
+}
+
 var mainCarouselOffers = new Carousel("mainCarouselOffers");
 mainCarouselOffers.addControl('prev');
 mainCarouselOffers.addControl('next');
-destinations.map(function (destination) {
-    mainCarouselOffers.addItem(destinationPane(destination, 'pages/booking.html', true));
+offers.map(function (offer) {
+    //console.log(offer);
+    mainCarouselOffers.addItem(offerPane(offer, 'pages/booking.html', true));
 });
+
+
+/*
+ {
+ "Name": "Josef",
+ "Destination": "India",
+ "Quote": "I didn't know what to expect before coming to India, but I came home fully satisfied after taking the India tour. The colors, the food, and the people of India are amazing, I'd love to go again someday!"
+ },
+ */
+function testimonialPane(testimonial) {
+
+
+    var name = testimonial.Name;
+    var destination = testimonial.Destination;
+    var paragraph = testimonial.Quote;
+    if ( removeDotsFromImageURL === true ) {
+        imageURL = imageURL.substring(3);
+    }
+    var element = $('<div class="item">');
+    var image = $('<img>').attr('src', imageURL).attr('alt', imageAlt);
+    var caption = $('<div class="carousel-caption">');
+    var link = $('<a>').attr('href', linkURL);
+    $('<h1>').text(name).appendTo(link);
+    $('<p>').text(paragraph).appendTo(link);
+    caption.append(link);
+    element.append(image).append(caption);
+    return element;
+
+}
+
+var mainCarouselTestimonials = new Carousel("mainCarouselTestimonials");
+mainCarouselTestimonials.addControl('prev');
+mainCarouselTestimonials.addControl('next');
+testimonials.map(function (testimonial) {
+    //console.log(testimonial);
+    mainCarouselTestimonials.addItem(offerPane(offer, 'pages/booking.html', true));
+});
+
---
+++ js/testimonials.js
@@ -0,0 +1,62 @@
+var testimonials = [
+    {
+        "Name": "Josef",
+        "Destination": "India",
+        "Quote": "I didn't know what to expect before coming to India, but I came home fully satisfied after taking the India tour. The colors, the food, and the people of India are amazing, I'd love to go again someday!"
+    },
+    {
+        "Name": "Stefan",
+        "Destination": "Iceland",
+        "Quote": "I didn't believe it before I went, but I actually had some amazing swimming with whales in Iceland on my summer trip. We saw so many unforgettable landscapes all around the country, sights I only could dream about before visiting Iceland!"
+    },
+    {
+        "Name": "Maria",
+        "Destination": "Thailand",
+        "Quote": "I was looking for cultural experience and some amazing beaches to relax in, and Thailand was all of that, and more! I wish I could find food like this back home in my village in Styria, but I guess I'll just have to go back again next year to do it all over again!"
+    },
+    {
+        "Name": "Eva",
+        "Destination": "United States",
+        "Quote": "I've seen America in the movies ever since I was a kid, so I knew I needed to go someday. Finally I got what I was looking for, and I even had the most unique BBQ in Texas that I could never experience back home!"
+    },
+    {
+        "Name": "Martyna",
+        "Destination": "Brazil",
+        "Quote": "I learned to dance Samba in Brazil, and had the most amazing time partying during Carnaval in Brazil. I'd love to go again next year, and stay even longer next time! The people are so friendly in Brazil.",
+    },
+    {
+        "Name": "Max",
+        "Destination": "Egypt",
+        "Quote": "Seeing the pyraminds and the Sphinx was on my bucket list ever since I read about them in history class when I was a teenager. Seeing them in real life is an experience I'll remember forever. The history of Egypt is unearthed in the desert and in museums, and I got to see all of it, I really felt like I was a king amongst the ruins!"
+    },
+    {
+        "Name": "Georg",
+        "Destination": "South Korea",
+        "Quote": "I got the most unique experience visiting the DMZ during my tour of South Korea. It is so interestesting to see such a pivotal place in history, I really feel like I'm a part of it all now!"
+    },
+    {
+        "Name": "Caroline",
+        "Destination": "Australia",
+        "Quote": "The cutest little kangaroo came up to me in Sydney and stole my sandwich! I laughed so much until I cried. That's when I knew it was the start of a special vacation. I took some amazing photos of the Sydney Opera House at sunset, all of my friends are liking it on facebook!"
+    },
+    {
+        "Name": "Andra",
+        "Destination": "France",
+        "Quote": "From the top of the Eiffel Tower, you can see all of Paris, and I'll never forget that. Our hotel had the cutest little boulangerie next door, every morning I would go down and have the best croissant of my life. I shopped until I dropped on the Champs D'Elysee and I'm saving up to go back again next year!"
+    },
+    {
+        "Name": "Melissa",
+        "Destination": "South Africa",
+        "Quote": "I hiked up both Table Mountain and Lion's Head in Cape Town, it was just what I needed to escape the busy city life back home. We even went into a cage to watch sharks in their natural habitat, I was scared for my life!"
+    },
+    {
+        "Name": "Sofia",
+        "Destination": "Portugal",
+        "Quote": "I ate the biggest sandwich of my life in Porto, it's called Francesinha! It's covered in sauce and so messy, but I was so full and satisfied afterwards! I learned so much about all the famous world explorers from Portugal, I want to be Magellan someday and circle the world by sea!"
+    },
+    {
+        "Name": "Jonas",
+        "Destination": "Mexico",
+        "Quote": "Never in my life will I eat so many tacos and drink so many shots of Tequila ever again. But I don't regret it one bit, trip of a lifetime!"
+    }
+];

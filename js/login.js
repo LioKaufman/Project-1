f06bc386455afb43b9022781778fa0277b667238
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-03-17 12:55:42 +0100

    login helper functions for local storage

--- js/login.js
+++ js/login.js
@@ -2,4 +2,36 @@
 // Simulating login functionality
 
 
+// helper function to retrieve customer data from local storage
+function getCustomerData() {
 
+    // check if customer data in local storage
+    if (localStorage.hasOwnProperty("customerData")){
+        return JSON.parse(localStorage.getItem("customerData"));
+    }
+
+    // if there isn't return empty customer object
+    return { firstName: "", lastName: "", eMail: "" }
+}
+
+// helper function to set customer data in local storage
+function setCustomerData(data) {
+    localStorage.setItem("customerData", JSON.stringify(data));
+}
+
+// helper function to clear customer data in local storage
+function clearCustomerData() {
+    localStorage.removeItem("customerData");
+}
+
+// execute when the dom is finished building
+// earliest point before for instance images are loaded
+// content will appear to be already there and not "pop in"
+$(document).on("ready", function () {
+
+    // store the current customer data into a variable so we only have to get the data once.
+    var customer = getCustomerData();
+    $(".formFirstName").val(customer.firstName);
+    $(".formLastName").val(customer.lastName);
+    $(".formEmail").val(customer.eMail);
+});
